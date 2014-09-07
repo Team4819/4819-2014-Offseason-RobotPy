00cf34826cc867da28d6a1cba472a6c44156a7de
Christian J. Balcom <robot.inventor@gmail.com>
2014-09-07 07:54:39 -0400

    Fixed random travis-cl errors

--- framework/tests/test_events.py
+++ framework/tests/test_events.py
@@ -26,19 +26,24 @@ def test_state_events():
     mod1 = modmaster.get_mod("test1")
     assert mod1.index is 1
     events.trigger("test", "Tester")
+    time.sleep(.1)
     assert mod1.index is 2
     mod1.reset()
     assert mod1.index is 1
     events.set_event("test", "Tester", True)
+    time.sleep(.1)
     assert mod1.index is 2
     mod1.reset()
     assert mod1.index is 1
     events.refresh_events("test1")
+    time.sleep(.1)
     assert mod1.index is 2
     events.set_event("test", "Tester", False)
+    time.sleep(.1)
     mod1.reset()
     assert mod1.index is 1
     events.refresh_events("test")
+    time.sleep(.1)
     assert mod1.index is 1
     modmaster.kill_all_mods()
     events.purge_events()
